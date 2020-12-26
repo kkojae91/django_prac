8e0884e236820a2104a9e0e128e0c84007719313
kkojae <jaejeung3210@gmail.com>
2020-12-26 18:10:01 +0900

    file 정리

--- bookmarkProject/bookmark/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from bookmark.models import Bookmark
-
-# Register your models here.
-
-@admin.register(Bookmark)
-class BookmarkAdmin(admin.ModelAdmin):
-  list_display = ('id', 'title', 'url')
